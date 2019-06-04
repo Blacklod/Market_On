5ea7a89f82e4f697cbd5086f7b96af5bd9a71f96
Admin <mivanov175@gmail.com>
2019-06-05 00:28:36 +0300

    NiH don't arbeiten

---
+++ src/main/java/servlets/MyServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        req.setAttribute("todo", "10");
+
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+}
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>Приветствуем тебя в нашем онлайн магазине!</title>
+    <title>!</title>
   </head>
   <body>
 
